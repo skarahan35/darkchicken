8835d299abf767aa1a06feda187f51f5882ded5e
Serkan Karahan <serkan.karahan@2ag.com.tr>
2024-02-20 22:12:39 +0300

    atom button css

--- dark-chicken-project/src/app/app.module.ts
+++ dark-chicken-project/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DCAtomModule } from 'projects/darkchicken';
+import { ButtonsComponent } from './buttons/buttons.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonsComponent
   ],
   imports: [
     BrowserModule,
