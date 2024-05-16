33674a6291391c1c0ee7bec593e268f5fd04042f
Ergül FERİK <eng.ergulferik@gmail.com>
2024-05-16 22:30:53 +0300

    button-presentation-page-redesigned-(contiune)

--- dark-chicken-project/src/app/buttons/buttons.component.ts
+++ dark-chicken-project/src/app/buttons/buttons.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TabModel } from 'projects/darkchicken/src/lib/models/dc-models.model';
+import { TableColumn } from 'projects/darkchicken/src/lib/types/table-column.type';
 import { buttonEventsHtml, buttonEventsTs, buttonSize, buttonWithTextButton, noDecorationButton, outlinedButton, passiveButton } from 'src/assets/codes/button';
 
 @Component({
@@ -8,6 +9,88 @@ import { buttonEventsHtml, buttonEventsTs, buttonSize, buttonWithTextButton, noD
   styleUrls: ['./buttons.component.css']
 })
 export class ButtonsComponent {
+
+  mainTabs: TabModel[] = [
+    {
+      tabId: 'overview',
+      tabTitle: 'Overview',
+    },
+    {
+      tabId: 'api',
+      tabTitle: 'API',
+      isActive:true
+    }, 
+    {
+      tabId: 'examples',
+      tabTitle: 'Examples',
+    }
+  ]
+
+  apiTableColumns1:TableColumn[]=[
+    {
+      prop:'input',
+      name:'Inputs',
+      sortable:false,
+      isEditable:false
+    },
+    {
+      prop:'des',
+      name:'Description',
+      sortable:false,
+      isEditable:false
+    }
+  ]
+
+  apiTableRows1:any=[
+    {
+      input:"input1",
+      des:"des1"
+    },
+    {
+      input:"input2",
+      des:"des2"
+    },
+    {
+      input:"input3",
+      des:"des3"
+    },
+    {
+      input:"input4",
+      des:"des4"
+    },
+    {
+      input:"input5",
+      des:"des5"
+    },
+    {
+      input:"input6",
+      des:"des6"
+    },
+    {
+      input:"input7",
+      des:"des7"
+    }
+  ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   tabs: TabModel[] = [{
     tabId: 'des',
@@ -18,45 +101,45 @@ export class ButtonsComponent {
     tabTitle: 'Code'
   },
   ]
-  codeTabs:TabModel[] = [
+  codeTabs: TabModel[] = [
     {
-      tabId:'html',
-      tabTitle:'HTML',
-      tabIcon:'html5',
+      tabId: 'html',
+      tabTitle: 'HTML',
+      tabIcon: 'html5',
     },
     {
-      tabId:'ts',
-      tabTitle:'TS',
-      tabIcon:'angular',
+      tabId: 'ts',
+      tabTitle: 'TS',
+      tabIcon: 'angular',
     }
   ]
-noDecorationButton = noDecorationButton
-buttonWithTextButton = buttonWithTextButton
-outlinedButton = outlinedButton
-passiveButton = passiveButton
-buttonEventsHtml = buttonEventsHtml
-buttonEventsTs = buttonEventsTs
-buttonSize = buttonSize
-  
-dcClick(e:any){
-  console.log({dcClick: e})
-}
-dcHover(e:any){
-  console.log({dcHover: e})
-}
-dcMouseUp(e:any){
-  console.log({dcMouseUp: e})
-}
-dcMouseDown(e:any){
-  console.log({dcMouseDown: e})
-}
-dcMouseOut(e:any){
-  console.log({dcMouseOut: e})
-}
-dcMouseEnter(e:any){
-  console.log({dcMouseEnter: e})
-}
-dcDoubleClick(e:any){
-  console.log({dcDoubleClick: e})
-}
+  noDecorationButton = noDecorationButton
+  buttonWithTextButton = buttonWithTextButton
+  outlinedButton = outlinedButton
+  passiveButton = passiveButton
+  buttonEventsHtml = buttonEventsHtml
+  buttonEventsTs = buttonEventsTs
+  buttonSize = buttonSize
+
+  dcClick(e: any) {
+    console.log({ dcClick: e })
+  }
+  dcHover(e: any) {
+    console.log({ dcHover: e })
+  }
+  dcMouseUp(e: any) {
+    console.log({ dcMouseUp: e })
+  }
+  dcMouseDown(e: any) {
+    console.log({ dcMouseDown: e })
+  }
+  dcMouseOut(e: any) {
+    console.log({ dcMouseOut: e })
+  }
+  dcMouseEnter(e: any) {
+    console.log({ dcMouseEnter: e })
+  }
+  dcDoubleClick(e: any) {
+    console.log({ dcDoubleClick: e })
+  }
 }
